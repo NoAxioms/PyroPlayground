f13f07d6a2b27b515b8624a9b74fc3fb05514b4d
Michael Fishman <michaeljfishman@gmail.com>
2019-03-07 06:53:11 -0500

    Tiger progress.

--- integer_line_mdp.py
+++ integer_line_mdp.py
@@ -1,5 +1,5 @@
 #Adapted from https://agentmodels.org/chapters/3a-mdp.html
-import os
+import os, time
 import json
 import collections
 import math
@@ -25,16 +25,20 @@ pyro.set_rng_seed(0)
 smoke_test = 'CI' in os.environ
 
 actions = [-1,0,1]
+goal_state = 3
+discount = 0.99
 def transition(state, action):
-	return state + action
+	return (state + action) % 4
 
 def utility(state, action):
-	return 1. if state==1 else -1.
-
+	return 1. if state==goal_state else -10.
+	
+@memoize
 def expected_utility(state, action, time_left):
 	r = utility(state, action)  #Immediate reward
 	new_time_left = time_left - 1
 	if new_time_left == 0:
+		# print("{} {} {}".format(action, state, r))
 		return r
 	else: #Get expected value of next state
 		#Get transition probs
@@ -48,30 +52,38 @@ def expected_utility(state, action, time_left):
 	    next_actions_dists = [act(ns, new_time_left) for ns in next_states]
 	    next_state_values = []
 	    for i in range(len(next_states)):
-	    	#The expected value is the expected expected expected utility, with expectation taken over next actions
-	    	nad, nav = next_actions_dists[i]._dist_and_values()
-	    	nad = nad.probs
-	    	nav = list(nav.values())
-	    	potential_expected_utilities = [expected_utility(next_states[i], a, new_time_left) for a in nav]
-	    	next_state_values.append(torch.dot(nad, torch.tensor(potential_expected_utilities)))
+	    	if next_states[i] == goal_state:
+	    		next_state_values.append(torch.tensor(0.0))
+    		else:
+		    	#The expected value is the expected expected expected utility, with expectation taken over next actions
+		    	nad, nav = next_actions_dists[i]._dist_and_values()
+		    	nad = nad.probs
+		    	nav = list(nav.values())
+		    	potential_expected_utilities = [expected_utility(next_states[i], a, new_time_left) for a in nav]
+		    	next_state_values.append(torch.dot(nad, torch.tensor(potential_expected_utilities)))
 	    # next_state_values = torch.tensor(data=[expected_utility(next_states[i],next_actions[i], new_time_left) for i in range(len(next_states))], dtype=torch.float32)
 	    # print("next_state_probs: {}".format(next_state_probs))
 	    # print("next_state_values: {}".format(next_state_values))
-	    return r + torch.dot(next_state_probs, torch.tensor(next_state_values))
+	    return r + discount * torch.dot(next_state_probs, torch.tensor(next_state_values))
 
 @Marginal
 def act(state, time_left):
 	action = actions[pyro.sample('a_id', dist.Categorical(torch.ones(len(actions))))]
 	eu = expected_utility(state, action, time_left)
-	factor('action_utility',10.0 * eu)
+	factor('action_utility',1000.0 * eu)
 	return action
 
-start_state = 0
-total_time = 6
+start_state = 0  #If start == goal, the actions don't matter and agent may act weird
+total_time = 7  #Can reach goal total_time - 1 distance away
+start_time = time.time()
 pd, pv = act(start_state, total_time)._dist_and_values()
+total_time = time.time() - start_time
+print("total_time: {}".format(total_time))
 print(pd.probs) 
 
 """
+When total_time = 1, only immediate reward is counted, so the value of the next state is ignored.
+When total_time is 2, the value of the next state is taken into account
 When goal is +-1 and total_time > 1, works correctly. When goal is 0 and total_time = 2, gives [.25, .5, .25]. For total_time != 2, uniform. For goal = 2, uniform.
 """
 
--- tiger.py
+++ tiger.py
@@ -1,4 +1,7 @@
-import os, json
+import os
+import json
+import collections
+import math
 
 import numpy as np
 import torch
@@ -8,8 +11,11 @@ import torchvision.transforms as transforms
 
 import pyro
 import pyro.distributions as dist
+import pyro.poutine as poutine
 from pyro.infer import SVI, Trace_ELBO
 from pyro.optim import Adam
+
+from search_inference import factor, HashingMarginal, memoize, Search, Marginal
 
 pyro.enable_validation(True)
 pyro.distributions.enable_validation(False)
@@ -17,17 +23,11 @@ pyro.set_rng_seed(0)
 # Enable smoke test - run the notebook cells on CI.
 smoke_test = 'CI' in os.environ
 
+tiger_locations = ['left', 'right']
+agent_locations = tiger_locations + ['trapped', 'free']
+State = collections.namedtuple("State",["tiger_location","agent_location"])
 observations = ["left", "right", "silence"]
-states = ["left", "right", "dead", "free"]
-actions = ["left", "right", "listen"]
-def sample_observation(state, action, roar_rate=.8, accuracy = .9):
-	if action == "listen":
-		hear_noise = pyro.sample('hear_noise', dist.Bernoulli(roar_rate))
-		if hear_noise:
-			roar_is_correct = pyro.sample('roar_is_correct',dist.Bernoulli(accuracy))
-			return state if roar_is_correct else [s for s in ["left","right"] if s != state][0]
-	return "silence"
-
+actions = ["go_left", "go_right", "listen"]
 def transition(state, action):
 	if action == "listen":
 		return state
@@ -35,6 +35,34 @@ def transition(state, action):
 		return "dead"
 	else:
 		return "free"
+
+def utility(state, action):
+	if state.tiger_location == state.agent_location:
+		return -1000.
+	if state.agent_location == "trapped":
+		return -1.
+	if state.agent_location == "free":
+		return 10.
+
+def sample_observation(state, action, roar_rate=.8, accuracy = .9):
+	if action == "listen":
+		hear_noise = pyro.sample('hear_noise', dist.Bernoulli(roar_rate))
+		if hear_noise:
+			roar_is_correct = pyro.sample('roar_is_correct',dist.Bernoulli(accuracy))
+			return state.tiger_location if roar_is_correct else [s for s in tiger_locations if s != state][0]
+	return "silence"
+
+def initial_belief():
+	tiger_location = tiger_locations[int(pyro.sample('tiger_location', dist.Bernoulli(0.5)))]
+	return State(tiger_location=tiger_location, agent_location='trapped')
+
+def update_belief(belief, action, observation):
+	def new_belief():
+		s_0 = belief()
+		s_1 = transition(s_0, action)
+		pyro.sample('obs', sample_observation, obs=observation)
+		return s_1
+	return new_belief
 
 def run(state):
 	#Condition on final state being "free", then infer actions
@@ -44,7 +72,11 @@ def run(state):
 
 
 def main():
-	start_state_id = pyro.sample("start_state_id",dist.Categorical(torch.ones([2])))
-	"""
-	Need to sample action
-	"""
+	# start_state_id = pyro.sample("start_state_id",dist.Categorical(torch.ones([2])))
+	b = initial_belief
+	b1 = update_belief(b, 'listen', 'left')
+	s = b1()
+	print(s)
+
+if __name__ == "__main__":
+	main()
